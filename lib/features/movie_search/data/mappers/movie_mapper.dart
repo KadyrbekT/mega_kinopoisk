a528a0165cbd48fe0ae198ab5fa465142c5fca10
TashtemirovK <tashtemirovkadyrbek@gmail.com>
2025-10-26 05:31:26 -0700

    check my line

--- lib/features/movie_search/data/mappers/movie_mapper.dart
+++ lib/features/movie_search/data/mappers/movie_mapper.dart
@@ -32,28 +32,3 @@ extension MovieDtoMapper on MovieDto {
     );
   }
 }
-
-
-// import '../models/movie_dto.dart';
-// import '../../domain/entities/movie.dart';
-//
-// extension MovieDtoMapper on MovieDto {
-//   Movie toDomain() {
-//     return Movie(
-//       id: id,
-//       title: name ?? alternativeName ?? enName ?? 'Без названия',
-//       originalTitle: enName,
-//       year: year,
-//       description: description,
-//       shortDescription: shortDescription,
-//       posterUrl: poster?.previewUrl ?? poster?.url,
-//       rating: rating?.kp ?? rating?.imdb ?? rating?.tmdb,
-//       genres: genres?.map((g) => g.name ?? '').where((e) => e.isNotEmpty).toList() ?? [],
-//       countries: countries?.map((c) => c.name ?? '').where((e) => e.isNotEmpty).toList() ?? [],
-//       persons: persons?.map((p) => p.name ?? '').where((e) => e.isNotEmpty).toList() ?? [],
-//       trailerUrl: videos?.trailers?.isNotEmpty == true ? videos!.trailers!.first.url : null,
-//       slogan: slogan,
-//       isSeries: isSeries ?? false,
-//     );
-//   }
-// }
